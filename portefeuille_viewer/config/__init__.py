96cf81e75bfd39df0c744400dd4f8ccdf07fe649
onno <onno@kievit.me>
2025-10-15 09:56:26 +0200

    Refactor: Config management naar settings.ini + config module
    - Nieuwe config/ map met settings.ini, settings_manager.py, __init__.py
    - Verplaats database config van hardcoded dict naar INI file
    - Settings tab uitgebreid met database management UI
    - Repository.py gebruikt nu dynamische config loading
    - Main_window.py verbonden met config changes signal
    - Oude config.py verwijderd
    - Backwards compatible via convenience functies
    - User settings in ~/.portefeuille_viewer/settings.ini

---
+++ portefeuille_viewer/config/__init__.py
@@ -0,0 +1,27 @@
+# portefeuille_viewer/config/__init__.py
+"""
+Configuration module voor Portefeuille Viewer.
+Centraliseert alle applicatie settings in settings.ini
+"""
+
+from .settings_manager import (
+    get_settings,
+    get_databases,
+    get_default_database,
+    add_database,
+    remove_database,
+    update_database,
+    set_default_database,
+    SettingsManager
+)
+
+__all__ = [
+    'get_settings',
+    'get_databases',
+    'get_default_database',
+    'add_database',
+    'remove_database',
+    'update_database',
+    'set_default_database',
+    'SettingsManager'
+]
