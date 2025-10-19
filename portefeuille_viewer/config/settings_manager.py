0c65cc17ace08ffdf8503468bbad30e709fbeffb
onno <onno@kievit.me>
2025-10-20 01:25:31 +0200

    sprinters toegevoegd, actieve price cache, div toegevoegd, portfolio tabel klaar in aandelen live tab + kleuren errors gefixed: - problemen met kolommen die niet bestaan, als er geen transacties van zijn geweest - error met databse, met de view op tabel div. aanklikken levert een corrupte database, waardoor excel niet meer werkt. fixen - settings pad gewijzigd naar de map waar in gewerkt wordt -

--- portefeuille_viewer/config/settings_manager.py
+++ portefeuille_viewer/config/settings_manager.py
@@ -10,7 +10,7 @@ from typing import Dict, Optional
 DEFAULT_CONFIG_PATH = Path(__file__).parent / "settings.ini"
 
 # Pad naar user config (in home directory)
-USER_CONFIG_DIR = Path(__file__).parent / ".portefeuille_viewer"
+USER_CONFIG_DIR = Path(__file__).parent / ".user_settings"
 USER_CONFIG_PATH = USER_CONFIG_DIR / "settings.ini"
 
 
