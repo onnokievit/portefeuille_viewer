d2c17311f58abaecd6a4531f0655c0a0bd0f6437
onno <onno@kievit.me>
2025-11-02 22:51:29 +0100

    optie eind functioneert, verplaats naar db / delete from db, db in app wordt geupdate middels signaal

--- portefeuille_viewer/devtools/test_loader.py
+++ portefeuille_viewer/devtools/test_loader.py
@@ -6,10 +6,7 @@ def test_load_open_opties():
     print(df.shape)
     print(df.columns)
     print(df.head())
-    df = repository.load_open_opties()
-    print(df.shape)
-    print(df.columns)
-    print(df.head())
+
 
 if __name__ == "__main__":
     test_load_open_opties()
