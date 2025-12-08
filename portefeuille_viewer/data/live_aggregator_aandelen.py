1dbc9b66d42f3fd818d39090a74bd98b0708ce57
onno <onno@kievit.me>
2025-12-09 00:59:19 +0100

    verdere optimalisatie signaal structuur. punt 2

--- portefeuille_viewer/data/live_aggregator_aandelen.py
+++ portefeuille_viewer/data/live_aggregator_aandelen.py
@@ -22,7 +22,7 @@ class LiveAggregatorAandelen(QObject):
         self._initialize_data()
         signals.snapshotUpdated.connect(self._on_snapshot_updated)
         signals.databaseChanged.connect(self._on_database_changed)
-        signals.ordersCommitted.connect(self.refresh_data)
+        #signals.ordersCommitted.connect(self.refresh_data)
         
     
     def _on_database_changed(self, db_name):
