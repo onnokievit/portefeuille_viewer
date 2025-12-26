3019494ee8ee1c6c3cfadac84281316257f7a0f4
onno <onno@kievit.me>
2025-12-26 21:34:31 +0100

    1 aanzet value per sector

--- portefeuille_viewer/ui/sector_tab.py
+++ portefeuille_viewer/ui/sector_tab.py
@@ -15,23 +15,32 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QHeaderView, QSizePolicy,
+    QTableView, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(1757, 1220)
-        self.pieChartValue1 = QFrame(Form)
-        self.pieChartValue1.setObjectName(u"pieChartValue1")
-        self.pieChartValue1.setGeometry(QRect(110, 90, 221, 181))
-        self.pieChartValue1.setFrameShape(QFrame.Shape.StyledPanel)
-        self.pieChartValue1.setFrameShadow(QFrame.Shadow.Raised)
-        self.frame_2 = QFrame(Form)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setGeometry(QRect(360, 90, 221, 181))
-        self.frame_2.setFrameShape(QFrame.Shape.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Shadow.Raised)
+        Form.resize(2500, 2000)
+        self.pieChartValueLineair = QFrame(Form)
+        self.pieChartValueLineair.setObjectName(u"pieChartValueLineair")
+        self.pieChartValueLineair.setGeometry(QRect(690, 10, 561, 481))
+        self.pieChartValueLineair.setFrameShape(QFrame.Shape.StyledPanel)
+        self.pieChartValueLineair.setFrameShadow(QFrame.Shadow.Raised)
+        self.pieChartValueLineairNaOpties = QFrame(Form)
+        self.pieChartValueLineairNaOpties.setObjectName(u"pieChartValueLineairNaOpties")
+        self.pieChartValueLineairNaOpties.setGeometry(QRect(1850, 10, 561, 481))
+        self.pieChartValueLineairNaOpties.setFrameShape(QFrame.Shape.StyledPanel)
+        self.pieChartValueLineairNaOpties.setFrameShadow(QFrame.Shadow.Raised)
+        self.pieChartValueLineairPutITM = QFrame(Form)
+        self.pieChartValueLineairPutITM.setObjectName(u"pieChartValueLineairPutITM")
+        self.pieChartValueLineairPutITM.setGeometry(QRect(1270, 10, 561, 481))
+        self.pieChartValueLineairPutITM.setFrameShape(QFrame.Shape.StyledPanel)
+        self.pieChartValueLineairPutITM.setFrameShadow(QFrame.Shadow.Raised)
+        self.tableSectorValuesLineair = QTableView(Form)
+        self.tableSectorValuesLineair.setObjectName(u"tableSectorValuesLineair")
+        self.tableSectorValuesLineair.setGeometry(QRect(10, 10, 661, 481))
 
         self.retranslateUi(Form)
 
